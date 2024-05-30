0eb07b6b4e6f8770902e5f339637adfad2e6d09c
1-21-01-E043-22\POO <alisson.viracocha@epn.edu.ec>
2024-05-30 08:36:42 -0500

    procesadores_intel2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import procesadores.PROCESADORES;
+import procesadores.procesadores_intel;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class Main {
         // Integran 1 Carlos (El Neeko)
 
         // Integrante 2 Josue Guerra
+        procesadores_intel proc = new procesadores_intel();
 
         // Integrante 3 Adrian Ramos (El keepchup)
 
