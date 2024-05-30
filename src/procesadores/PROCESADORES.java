3f27c91b6d428f96a2fad44ad3ade96bd9503875
Luis Adrian Ramos <luchoramos042004@gmail.com>
2024-05-30 08:08:58 -0500

    DATOS

--- src/procesadores/PROCESADORES.java
+++ src/procesadores/PROCESADORES.java
@@ -1,4 +1,60 @@
 package procesadores;
 
 public class PROCESADORES {
+    String nombre;
+    int nucleos;
+    double tdp, velocidad;
+    String arquitectura;
+
+    public PROCESADORES() {
+    }
+
+    public PROCESADORES(String nombre, int nucleos, double tdp, double velocidad, String arquitectura) {
+        this.nombre = nombre;
+        this.nucleos = nucleos;
+        this.tdp = tdp;
+        this.velocidad = velocidad;
+        this.arquitectura = arquitectura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNucleos() {
+        return nucleos;
+    }
+
+    public void setNucleos(int nucleos) {
+        this.nucleos = nucleos;
+    }
+
+    public double getTdp() {
+        return tdp;
+    }
+
+    public void setTdp(double tdp) {
+        this.tdp = tdp;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public String getArquitectura() {
+        return arquitectura;
+    }
+
+    public void setArquitectura(String arquitectura) {
+        this.arquitectura = arquitectura;
+    }
 }
+
