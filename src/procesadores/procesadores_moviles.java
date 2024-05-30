6efc4a23120234c5751f081c745e7438df9e8535
Luis Adrian Ramos <luchoramos042004@gmail.com>
2024-05-30 08:19:17 -0500

    ProMoviles

---
+++ src/procesadores/procesadores_moviles.java
@@ -0,0 +1,24 @@
+package procesadores;
+
+public class procesadores_moviles extends PROCESADORES{
+    Boolean eficiencia;
+
+    public procesadores_moviles() {
+    }
+
+    public procesadores_moviles(Boolean eficiencia) {
+        this.eficiencia = eficiencia;
+    }
+
+    public Boolean getEficiencia() {
+        return eficiencia;
+    }
+
+    public void setEficiencia(Boolean eficiencia) {
+        this.eficiencia = eficiencia;
+    }
+
+    public void ahorro_Energia(){
+
+    }
+}
